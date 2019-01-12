ed87277234b2f947b2499f0bbfb1bcd88f30daf9
github_Lee <9787206@qq.com>
2019-01-12 23:14:44 +0800

    update StudentDao

--- src/main/java/cn/mln521/mybatis01/dao/StudentDao.java
+++ src/main/java/cn/mln521/mybatis01/dao/StudentDao.java
@@ -5,6 +5,7 @@ import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 
+import java.io.IOException;
 import java.io.InputStream;
 
 /**
@@ -21,33 +22,46 @@ public class StudentDao {
      */
     public Student selectStudentById(int id) {
 
-        //使用类加载器加载mybatis的配置文件（它也加载关联的映射文件）
-        InputStream stream=StudentDao.class.getClassLoader().getResourceAsStream("conf.xml");
-        //构建sqlSession的工厂
-        SqlSessionFactory ssf=new SqlSessionFactoryBuilder().build(stream);
-        //使用MyBatis提供的Resources类加载mybatis的配置文件（它也加载关联的映射文件）
-        //Reader reader = Resources.getResourceAsReader(resource);
-        //构建sqlSession的工厂
-        //SqlSessionFactory sessionFactory = new SqlSessionFactoryBuilder().build(reader);
+        InputStream inputStream = null;
+        SqlSessionFactory sqlSessionFactory = null;
+        SqlSession session = null;
+        Student student = null;
+        try {
+            //使用类加载器加载mybatis的配置文件（它也加载关联的映射文件）
+            inputStream = StudentDao.class.getClassLoader().getResourceAsStream("conf.xml");
+            //构建sqlSession的工厂
+            sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+            //使用MyBatis提供的Resources类加载mybatis的配置文件（它也加载关联的映射文件）
+            //Reader reader = Resources.getResourceAsReader(resource);
+            //构建sqlSession的工厂
+            //SqlSessionFactory sessionFactory = new SqlSessionFactoryBuilder().build(reader);
 
-        //创建能执行映射文件中sql的sqlSession
-        SqlSession session = ssf.openSession();
-        /**
-         * 映射sql的标识字符串，
-         * com.zhangguo.mybatis01.dao.studentMapper是studentMapper.xml文件中mapper标签的namespace属性的值，
-         * selectStudentById是select标签的id属性值，通过select标签的id属性值就可以找到要执行的SQL
-         */
-        Student student = session.selectOne("cn.mln521.mybatis01.dao.studentMapper.selectStudentById", id);
+            //创建能执行映射文件中sql的sqlSession
+            session = sqlSessionFactory.openSession();
+            /**
+             * 映射sql的标识字符串，
+             * com.zhangguo.mybatis01.dao.studentMapper是studentMapper.xml文件中mapper标签的namespace属性的值，
+             * selectStudentById是select标签的id属性值，通过select标签的id属性值就可以找到要执行的SQL
+             */
+            student = session.selectOne("cn.mln521.mybatis01.dao.studentMapper.selectStudentById", id);
 
-        // 关闭会话
-        session.close();
+        }
+        finally {
+            if (session != null) {
+                // 关闭会话
+                session.close();
+            }
+            if (inputStream != null) {
+                try {
+                    inputStream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+        }
         return student;
 
     }
 
-//    public static void main(String[] args) {
-//        StudentDao studentDao = new StudentDao();
-//        Student student = studentDao.selectStudentById(1);
-//        System.out.println(student);
-//    }
 }
